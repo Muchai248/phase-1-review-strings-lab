fee9704999c97c1e4cee3f64473a1f1d2c975413
Mark Muchai <muchaimark6@gmail.com>
2023-06-13 20:31:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Mark Muchai';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+'Mark muchai'.slice();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
